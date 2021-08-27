9073f55f539e0478ff694a5f3e0f52c8774f2387
retgits <lstigter@gmail.com>
2021-08-27 12:45:48 -0700

    feat(brew): add new workflow to update formula

---
+++ scripts/script.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import requests as req
+import subprocess
+
+scriptlines = {
+    "version": ["16",'    version \\"{0}\\"'],
+    "darwinamd64": ["24", '            sha256 \\"{0}\\"'],
+    "darwinarm64": ["27", '            sha256 \\"{0}\\"'],
+    "linuxamd64": ["33", '        sha256 \\"{0}\\"']
+}
+
+def parseLine(line):
+    elems = line.split(' ')
+    sha = elems[0].strip()
+    elems = elems[2].split('_')
+    os = elems[1]
+    proc = elems[2]
+    version = '.'.join(elems[3].split('.')[0:3])
+    return {
+        "sha": sha,
+        "os": os,
+        "processor": proc,
+        "version": version
+    }
+
+url = 'https://downloads.akkaserverless.com/latest/checksums.txt'
+resp = req.get(url)
+
+lines = resp.text.splitlines()
+
+for line in lines:
+    d = parseLine(line)
+    if d['os'] + d['processor'] in scriptlines:
+        ## Update the SHA
+        ln = scriptlines[d['os'] + d['processor']][0]
+        ct = scriptlines[d['os'] + d['processor']][1].format(d['sha']) 
+        subprocess.call(["sed -i \"{0}s/.*/{1}/\" Formula/akkasls.rb".format(ln, ct)], shell=True)
+        ## Update the version
+        ln = scriptlines['version'][0]
+        ct = scriptlines['version'][1].format(d['version'])
+        subprocess.call(["sed -i \"{0}s/.*/{1}/\" Formula/akkasls.rb".format(ln, ct)], shell=True)
