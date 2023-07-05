53dff25c1b9d59801ca0634cbf73432bfe9bf88f
GitHub <noreply@github.com>
2023-07-05 17:10:22 +0000

    feat(cli): update kalix CLI to the latest stable version

--- Formula/kalix.rb
+++ Formula/kalix.rb
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 class Kalix < Formula
-    version "2.0.24"
+    version "2.0.25"
     desc "Kalix :: Command Line Interface"
     homepage "https://kalix.io"
     license ""
@@ -21,16 +21,16 @@ class Kalix < Formula
     if OS.mac?
         if Hardware::CPU.intel?
             url "https://downloads.kalix.io/#{version}/kalix_darwin_amd64_#{version}.tar.gz"
-            sha256 "bbebf6956830dbdfbba49a336f37298c534b46bfb491874a5cc1a5dfa77b559e"
+            sha256 "7af52d3c06ef9bd767a2f6e07e685d134a9f92eb9047a785cb3ba5e644eaa3da"
         else
             url "https://downloads.kalix.io/#{version}/kalix_darwin_arm64_#{version}.tar.gz"
-            sha256 "046474c2763d643cb73c31efe7fb70fc7b89e80f48988aa9c8b72e1e023a2c88"
+            sha256 "6c4614a9bf25b230edf2e3da56a7f98985565b43e61c8f95da0a3eb137a837d1"
         end
     end
 
     if OS.linux?
         url "https://downloads.kalix.io/#{version}/kalix_linux_amd64_#{version}.tar.gz"
-        sha256 "c13636ba5616fe63d7d9d807fa4cbe2632c067c0325657ea77c8e97a076d4115"
+        sha256 "2703ae8913aac70e926d8a5dc05f0ac6d94e5ee6f90beb5ea5d4914102cefa92"
     end
 
     def install
